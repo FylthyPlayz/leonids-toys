6283c56bdd89a0ea6ff1975d1a0368d632eb6f93
FylthyPlayz <jacob_cook_football@yahoo.com>
2021-10-05 15:48:03 -0500

    new toys added

--- leonids-toys.js
+++ leonids-toys.js
@@ -2,29 +2,29 @@ const toyToFind = 2
 
 const toys = [
     {
-    id: 1,
-    name: "PlayStation 5",
-    maker: "Sony",
-    quantity: 7,
-    price: 499.99,
-    stock: false
-},
-{
-    id: 2,
-    name: "Gloomhaven",
-    maker: "Cephalofair Games",
-    quantity: 22,
-    price: 199.99,
-    stock: true
-},
-{
-    id: 3,
-    name: "Dungeons and Dragons: 5th edition",
-    maker: "Wizards of the Coast",
-    quantity: 20,
-    price: 99.99,
-    stock: true
-}
+        id: 1,
+        name: "PlayStation 5",
+        maker: "Sony",
+        quantity: 7,
+        price: 499.99,
+        stock: false
+    },
+    {
+        id: 2,
+        name: "Gloomhaven",
+        maker: "Cephalofair Games",
+        quantity: 22,
+        price: 199.99,
+        stock: true
+    },
+    {
+        id: 3,
+        name: "Dungeons and Dragons: 5th edition",
+        maker: "Wizards of the Coast",
+        quantity: 20,
+        price: 99.99,
+        stock: true
+    }
 ]
 
 
@@ -43,17 +43,17 @@ const wardedMan = {
     quantity: 25,
     price: 59.99,
     stock: true
-    
+
 }
 toys.push(beyblade)
 toys.push(wardedMan)
 
 
 const addToyToInventory = (toyObject) => {
-const lastIndex = toys.length -1
-const currentLastToy = toys[lastIndex]
-const maxID = currentLastToy.id
-const idForNewToy = maxID + 1
+    const lastIndex = toys.length - 1
+    const currentLastToy = toys[lastIndex]
+    const maxID = currentLastToy.id
+    const idForNewToy = maxID + 1
     toyObject.id = idForNewToy
     toys.push(toyObject)
 }
@@ -69,8 +69,9 @@ const legendOfDrizzt = {
 addToyToInventory(legendOfDrizzt)
 
 for (const toy of toys) {
-    if (toy.id ===toyToFind) {
+    if (toy.id === toyToFind) {
         toy.price = toy.price + toy.price * .05
+    }
     console.log(`The price of the ${toy.name} is $${toy.price} and it is made by ${toy.maker}.`)
-}
+
 }
